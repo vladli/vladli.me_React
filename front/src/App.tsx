c752a966638579b99f1f3fb13211b72b3bb6a159
LI VLADISLAV <vladislav.li911@gmail.com>
2022-10-27 09:30:37 +0900

    1

---
+++ front/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <h1 className="bg-violet-400 text-3xl font-bold underline">Hello world!</h1>
+  );
+}
+
+export default App;

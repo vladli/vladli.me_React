c52652b06edfd72c71ac7a16e25ad00350cabca7
LI VLADISLAV <vladislav.li911@gmail.com>
2022-12-19 17:28:42 +0900

    1

--- front/src/components/Divider.tsx
+++ front/src/components/Divider.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Divider = () => {
-  return <hr className="my-4 h-px border-0 bg-gray-200 dark:bg-gray-700" />;
+interface IDivider {
+  vertical?: boolean;
+}
+const Divider: React.FC<IDivider> = ({ vertical }) => {
+  if (vertical) {
+    return <div className="w-px border-0 bg-gray-200 dark:bg-gray-700" />;
+  } else {
+    return <hr className="my-4 h-px border-0 bg-gray-200 dark:bg-gray-700" />;
+  }
 };
 
 export default Divider;
--- front/src/pages/auth/Login.tsx
+++ front/src/pages/auth/Login.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 import Card from "../../components/Cards/Card";
+import Divider from "../../components/Divider";
 
 const Login = () => {
   return (
-    <div>
-      <Card>
-        <Card.Body>asd</Card.Body>
-      </Card>
+    <div className="flex h-[100vh] items-center justify-center">
+      <div className="w-[80%]">
+        <Card>
+          <div className="flex h-[40vh]">
+            <div className="flex w-[50%] items-center justify-center ">
+              IMAGE
+            </div>
+            <Divider vertical />
+            <div className="flex w-[50%] items-center justify-center ">
+              Text
+            </div>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 };

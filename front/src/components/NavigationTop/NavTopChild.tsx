fcc4f71c9ac063dc93052b3332d72ee34d117f3b
LI VLADISLAV <vladislav.li911@gmail.com>
2023-01-29 23:32:50 +0900

    1

--- front/src/components/NavigationTop/NavTopChild.tsx
+++ front/src/components/NavigationTop/NavTopChild.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const NavTopChild = ({ items }: any) => {
   return (
-    <div className="z-10 rounded-lg bg-menuBgDark p-2">
+    <div className="z-10  rounded-lg bg-menuBgDark p-2">
       {items.map((item: any) => (
         <Link key={item.name} to={item.link}>
-          <div className="hover:text-gray-400">{item.name}</div>
+          <div className="rounded-lg pl-2 hover:bg-bgDark">{item.name}</div>
         </Link>
       ))}
     </div>

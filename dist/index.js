a7a01c491917caee318bcd7b987e463201c10e92
LI VLADISLAV <vladislav.li911@gmail.com>
2023-02-17 16:14:35 +0900

    typescript

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import express from "express";
+import { fileURLToPath } from "url";
+//import { verifyToken } from "./security/verifyToken.js";
+dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+//const __dirname = dirname(__filename);
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: ["http://localhost:3000", "https://vladli.me"] }));
+//app.use("/api", verifyToken, routes);
+// app.use(express.static(resolve(__dirname, "front/build")));
+// app.get("*/", (req: Request, res: Response) => {
+//   res.sendFile(path.resolve(__dirname, "front/build", "index.html"));
+// });
+/** Server */
+const PORT = process.env.PORT ?? 5000;
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+mongoose.set("strictQuery", true);
+mongoose
+    .connect(process.env.DB_CONNECT, {})
+    .then(() => console.log("MongoDB connected."))
+    .catch((error) => console.log(error));
--- index.ts
+++ index.ts
@@ -1,29 +1,27 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
-import bodyParser from "body-parser";
-import express from "express";
-import routes from "./routes/index.js";
+import express, { Express, Request, Response } from "express";
+//import routes from "./routes/index.js";
 import path, { resolve, dirname } from "path";
 import { fileURLToPath } from "url";
-import { verifyToken } from "./security/verifyToken.js";
+//import { verifyToken } from "./security/verifyToken.js";
 
 dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const app = express();
+//const __dirname = dirname(__filename);
+const app: Express = express();
 
-app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors({ origin: ["http://localhost:3000", "https://vladli.me"] }));
 
-app.use("/api", verifyToken, routes);
+//app.use("/api", verifyToken, routes);
 
-app.use(express.static(resolve(__dirname, "front/build")));
-app.get("*/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "front/build", "index.html"));
-});
+// app.use(express.static(resolve(__dirname, "front/build")));
+// app.get("*/", (req: Request, res: Response) => {
+//   res.sendFile(path.resolve(__dirname, "front/build", "index.html"));
+// });
 
 /** Server */
 
@@ -32,6 +30,6 @@ app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.DB_CONNECT, {})
+  .connect(process.env.DB_CONNECT!, {})
   .then(() => console.log("MongoDB connected."))
   .catch((error) => console.log(error));
